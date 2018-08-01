fa6942be491d9af259633816a4d62fba11aa178e
Graham Ashton <graham@effectif.com>
2018-08-01 22:10:46 +0100

    WIP: Started heading off course on a hack

--- decode-table-entry.py
+++ decode-table-entry.py
@@ -12,21 +12,36 @@ def split_second_byte(hex_string):
     return value & 0b00111111, value & 0b11000000
 
 
+def convert_chs_bytes(*hex_strings):
+    head = hex_to_int(hex_strings[0])
+    sector, cylinder = split_second_byte(hex_strings[1])
+    cylinder = cylinder + hex_to_int(hex_strings[2])
+
+
 def convert_to_lba_address(cylinder, head, sector):
     num_heads = 255
     sectors_per_track = 63
-    return ((cylinder * num_heads) + head) * sectors_per_track + sector
+    return ((cylinder * num_heads) + head) * sectors_per_track + sector - 1
 
 
-def main(head, byte2, byte3):
-    head = hex_to_int(head)
-    sector, cylinder = split_second_byte(byte2)
-    cylinder = cylinder + hex_to_int(byte3)
+def main(hex_codes):
+    bootable = hex_codes[0]
+    start_chs = hex_codes[1:4]
+    partition_type = hex_codes[4]
+    end_chs = hex_codes[5:8]
+    starting_sector = hex_codes[8:12]
+    partition_size = hex_codes[12:16]
 
-    address = convert_to_lba_address(cylinder, head, sector)
+    print "Bootable: %s [80 indicates bootable]" % bootable
+    print start_chs
 
-    print "CHS: %s, %s, %s => %s" % (cylinder, head, sector, address)
+    #  sector, cylinder = split_second_byte(byte2)
+    #  cylinder = cylinder + hex_to_int(byte3)
+
+    #  address = convert_to_lba_address(cylinder, head, sector)
+
+    #  print "CHS: %s, %s, %s => %s" % (cylinder, head, sector, address)
 
 
 if __name__ == '__main__':
-    main(*sys.argv[1:])
+    main(sys.argv[1:])
